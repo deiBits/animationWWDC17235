a2deb0f9b575770f572205c329b72dc9488d86f8
nbalasundara <nbalasundara@gmail.com>
2022-04-17 14:34:42 -0400

    Initialized from https://developer.apple.com/wwdc17/235  but does not work as yet

---
+++ Emoji/EmojiView.swift
@@ -0,0 +1,39 @@
+//
+//  EmojiView.swift
+//  Emoji
+//
+//  Created by natarajan b on 4/17/22.
+// https://developer.apple.com/wwdc17/235
+
+import UIKit
+class EmojiView: UIView {
+    let emojilayers: [CALayer]
+    
+    override init(frame: CGRect) {
+        let emojiList="😀😃😄😁😆🥹😅😂🤣🥲☺️😊😇🙂🙃😉😌😍🥰😘😗😙😚😋😛😝😜🤪🤨🧐🤓😎🥸🤩🥳😏😒😞😔😟😕🙁☹️😣😖😫😩🥺😢😭😤😠"
+        let screenScale = UIScreen.main.scale
+        let cornerInset = 45 // starting x/y position for the grid
+        let layerSize = 70
+        var index = 0 // to kéep track of what column / row we're on
+        var emojiLayers: [CALayer] = []
+        for e in emojiList {
+            let layer = CATextLayer()
+            layer.string = String(e)
+            layer.fontSize = 50
+            layer.contentsScale = screenScale
+            layer.bounds = CGRect (x: 0, y: 0, width: layerSize, height: layerSize)
+            layer.alignmentMode = CATextLayerAlignmentMode.center
+            let column = index % 4
+            let row = (index - column) / 4
+            layer.position = CGPoint (x: cornerInset + layerSize * column, y: cornerInset + layerSize * row)
+            emojiLayers.append(layer)
+            index = index + 1
+        }
+        self.emojilayers = emojiLayers
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Emoji/ViewController.swift
+++ Emoji/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import QuartzCore
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController : UIViewController {
+    var containerView : EmojiView?
+    let maxContainerSize = CGSize (width: 300, height: 540)
+    var currentContainerExpansion: Double = 0 {
+        didSet {
+            containerView?.layer.timeOffset = currentContainerExpansion
+        }
     }
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.black
+        let containerFrame = CGRect(origin: CGPoint(x: view.bounds.midX - maxContainerSize.width / 2, y:view.bounds.midY - maxContainerSize.height / 2), size: maxContainerSize)
+        let containerView = EmojiView(frame: containerFrame)
+        view.addSubview(containerView)
+        self.containerView = containerView
+        setupAnimations()
+        let rec = UIPanGestureRecognizer(target: self, action: #selector (handlePan))
+        view.addGestureRecognizer(rec)
+    }
+    
+    @objc func handlePan(_ recognizer: UIPanGestureRecognizer){
+        let dragDistanceY = recognizer.translation(in: view).y
+        let scaledDragAmount = Double(dragDistanceY / maxContainerSize.height)
+        // scale from ~..height to ~0…1
+        currentContainerExpansion = min(max (currentContainerExpansion + scaledDragAmount, 0), 1)
+        // clamp to 0-1 range so we don't have to set a fill mode on the main animation
+        recognizer.setTranslation(.zero, in: view)
+    }
+    
+    func setupAnimations() {
+        if let containerLayer = containerView?.layer, let emojiLayers = containerView?.emojilayers {
+            containerLayer.speed = 0
+            let animation = CABasicAnimation(keyPath: "bounds.size.height")
+            animation.fromValue = 80
+            animation.toValue = self.maxContainerSize.height
+            animation.duration = 1
+            animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+            let widthAnimation = CABasicAnimation(keyPath: "bounds.size.width")
+            widthAnimation.fromValue = 80
+            widthAnimation.toValue = self.maxContainerSize.width
+            widthAnimation.duration = 0.2
+            widthAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+            containerLayer.add(animation, forKey: nil)
+            containerLayer.add (widthAnimation, forKey: nil)
 
-
+            let baseStartTime = containerLayer.convertTime(CACurrentMediaTime(),from:nil)
+            for i in emojiLayers.indices {
+                let layer = emojiLayers[i]
+                let animation = CABasicAnimation(keyPath: "transform.scale.xy")
+                animation.fromValue = 0.01
+                animation.toValue = 1
+                animation.duration = 0.1
+                animation.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeOut)
+                animation.beginTime = baseStartTime + 0.028 * Double(i) // start each animation slightly later
+                animation.fillMode = CAMediaTimingFillMode.backwards // apply the initial value before the animation
+                layer.add(animation, forKey: nil)
+            }
+        }
+    }
 }
-
